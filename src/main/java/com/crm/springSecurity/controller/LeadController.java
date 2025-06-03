8c721cb7c432645cce6d439c9019dc202a690d5f
DaniloPelusci <danilopelusci@gmail.com>
2025-06-03 11:22:07 -0300

    ajustando o cors e o authentication

--- src/main/java/com/crm/springSecurity/controller/LeadController.java
+++ src/main/java/com/crm/springSecurity/controller/LeadController.java
@@ -12,13 +12,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/leads")
 public class LeadController {
